613bac1a87913c2111732ac3eca0fa8956593639
Jan Vorac <vorac@mail.muni.cz>
2020-05-01 19:20:20 +0200

    animation works

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,38 @@ const model = new Model(lambda0, fwhm, 10, 20);
 const plotMaker = new PlotMaker(model.counts, model.bins, "#histogram-container", 480, 200)
 
 document.getElementById("add-photons").addEventListener("click", addPhotonsClicked);
+
+/*
 function addPhotonsClicked() {
   const numPhotons = 10
   for (let i = 0; i < numPhotons; i++) {
+    const timer = setInterval(() => {
+      const binIndex = model.addPhoton()
+      if (binIndex) {
+        plotMaker.addPhoton(binIndex)
+      }
+    }, 50)
+  }
+}
+*/
+
+function addPhotonsClicked() {
+  const numPhotons = 10;
+  let photonsAdded = 0;
+  const timer = setInterval(() => {
     const binIndex = model.addPhoton()
     if (binIndex) {
       plotMaker.addPhoton(binIndex)
     }
-  }
+    photonsAdded += 1;
+    if (photonsAdded >= numPhotons) {
+      clearInterval(timer)
+    }
+
+  }, 50)
+
 }
+
+
 //console.log(model.photons)
 //console.log(hist(model.photons))
